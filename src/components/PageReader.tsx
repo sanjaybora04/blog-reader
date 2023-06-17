76e37394ecf835da2b5e5f17d96341bf2411f44d
Sanjay Bora <sanjaybora380@gmail.com>
2023-06-17 19:53:06 +0530

    commit

--- src/components/PageReader.tsx
+++ src/components/PageReader.tsx
@@ -12,7 +12,7 @@ export const PageReader = () => {
 
     function populateVoices() {
         const voices = speechSynthesis.getVoices();
-        const voicesSelect = document.getElementById('voices');
+        const voicesSelect = document.getElementById('page-reader-voices');
         if (voicesSelect) {
             voicesSelect.innerHTML = "";
 
@@ -50,11 +50,11 @@ export const PageReader = () => {
 
     function play() {
         playing = !playing
-        document.getElementById('controls')?.classList.toggle('playing')
+        document.getElementById('page-reader-controls')?.classList.toggle('playing')
         if (playing) {
             const voices = speechSynthesis.getVoices();
-            const voiceElement = document.getElementById('voices');
-            const speedElement = document.getElementById('speed');
+            const voiceElement = document.getElementById('page-reader-voices');
+            const speedElement = document.getElementById('page-reader-speed');
             const voice = voiceElement instanceof HTMLSelectElement ? parseFloat(voiceElement.value) : undefined;
             const speed = speedElement instanceof HTMLSelectElement ? parseFloat(speedElement.value) : undefined;
             if (voice == undefined || speed == undefined) {
@@ -63,8 +63,8 @@ export const PageReader = () => {
             }
             utterance.voice = voices[voice];
             utterance.rate = speed;
-            document.getElementById('controls')?.classList.add("fixed", "bottom-2", "right-1/2", "translate-x-1/2");
-            document.getElementById('stop')?.classList.remove('hidden')
+            document.getElementById('page-reader-controls')?.classList.add("fixed", "bottom-2", "right-1/2", "translate-x-1/2");
+            document.getElementById('page-reader-stop')?.classList.remove('hidden')
             speakLine();
             update();
         } else {
@@ -79,8 +79,8 @@ export const PageReader = () => {
         for (let i = 0; i < lines.length; i++) {
             lines[i].classList.remove("bg-sky-200");
         }
-        document.getElementById('controls')?.classList.remove("fixed", "bottom-2", "right-1/2", "translate-x-1/2", "playing");
-        document.getElementById('stop')?.classList.add('hidden')
+        document.getElementById('page-reader-controls')?.classList.remove("fixed", "bottom-2", "right-1/2", "translate-x-1/2", "playing");
+        document.getElementById('page-reader-stop')?.classList.add('hidden')
         index = 0
         position = 0
     }
@@ -128,16 +128,17 @@ export const PageReader = () => {
             position = e.charIndex;
         });
 
-
+        // handle Click outside the toggle window
         function handleClick(e: Event) {
             const target = e.target
             if (target instanceof Element) {
-                if (!target.closest('#toggleoptions, #options')) {
+                if (!target.closest('#page-reader-toggleoptions, #page-reader-options')) {
                     setOptions(false)
                 }
             }
         }
 
+        // when voice rate is changed
         function handleSpeedChange(e: Event) {
             const target = e.target
             if (target instanceof HTMLSelectElement) {
@@ -147,8 +148,7 @@ export const PageReader = () => {
             }
         }
 
-        document.getElementById('speed')?.addEventListener('change', handleSpeedChange);
-
+        // when voice is changed
         function handleVoicesChange(e: Event) {
             const target = e.target
             if (target instanceof HTMLSelectElement) {
@@ -158,36 +158,42 @@ export const PageReader = () => {
                 update();
             }
         }
-
-        document.getElementById('voices')?.addEventListener('change', handleVoicesChange);
-
+        
+        
+        // Keyboard and media button events
         function handleKeyDown(e: KeyboardEvent) {
             e.preventDefault();
-            if (e.which === 32 || e.which === 179) {
+            if (e.which === 32 || e.which === 179) {             // space and play button
                 play();
-            } else if (e.which === 39 || e.which === 176) {
+            } else if (e.which === 39 || e.which === 176) {      // right arrow key and next button
                 next();
-            } else if (e.which === 37 || e.which === 177) {
+            } else if (e.which === 37 || e.which === 177) {      // left arrow key and previous button
                 previous();
             }
         }
-
+        
         speechSynthesis.onvoiceschanged = (): void => {
             populateVoices();
         };
-
+        
         document.addEventListener('click', handleClick);
         document.addEventListener('keydown', handleKeyDown);
-
+        document.getElementById('page-reader-speed')?.addEventListener('change', handleSpeedChange);
+        document.getElementById('page-reader-voices')?.addEventListener('change', handleVoicesChange);
+        
+        
         return () => {
             utterance.removeEventListener('boundary', function (e) {
                 position = e.charIndex;
             });
             document.removeEventListener('click', handleClick);
             document.removeEventListener('keydown', handleKeyDown);
+            document.getElementById('page-reader-speed')?.removeEventListener('change', handleSpeedChange);
+            document.getElementById('page-reader-voices')?.removeEventListener('change', handleVoicesChange);
         };
     }, []);
 
+    // Check browser compatibility
     useEffect(() => {
         if ('speechSynthesis' in window && 'SpeechSynthesisUtterance' in window) {
             populateVoices()
@@ -198,18 +204,18 @@ export const PageReader = () => {
 
     return (
         <div>
-            <div id="controls" className="group flex w-max h-14 mx-auto bg-gray-700 text-white text-lg rounded-lg space-x-5 p-3">
-                <button type="button" title="previous" id="previous" className="hover:scale-125 focus:outline-none hover:outline-none" onClick={() => { previous() }}>
+            <div id="page-reader-controls" className="group flex w-max h-14 mx-auto bg-gray-700 text-white text-lg rounded-lg space-x-5 p-3">
+                <button type="button" title="page-reader-previous" id="page-reader-previous" className="hover:scale-125 focus:outline-none hover:outline-none" onClick={() => { previous() }}>
                     <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
                         <path strokeLinecap="round" strokeLinejoin="round" d="M21 16.811c0 .864-.933 1.405-1.683.977l-7.108-4.062a1.125 1.125 0 010-1.953l7.108-4.062A1.125 1.125 0 0121 8.688v8.123zM11.25 16.811c0 .864-.933 1.405-1.683.977l-7.108-4.062a1.125 1.125 0 010-1.953L9.567 7.71a1.125 1.125 0 011.683.977v8.123z" />
                     </svg>
                 </button>
-                <button type="button" title="stop" id="stop" className="hidden hover:scale-125 focus:outline-none" onClick={() => { stop() }}>
+                <button type="button" title="page-reader-stop" id="page-reader-stop" className="hidden hover:scale-125 focus:outline-none" onClick={() => { stop() }}>
                     <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
                         <path strokeLinecap="round" strokeLinejoin="round" d="M5.25 7.5A2.25 2.25 0 017.5 5.25h9a2.25 2.25 0 012.25 2.25v9a2.25 2.25 0 01-2.25 2.25h-9a2.25 2.25 0 01-2.25-2.25v-9z" />
                     </svg>
                 </button>
-                <button type="button" title="play" id="play" className="hover:scale-125 focus:outline-none" onClick={() => { play() }}>
+                <button type="button" title="page-reader-play" id="page-reader-play" className="hover:scale-125 focus:outline-none" onClick={() => { play() }}>
                     <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 group-[.playing]:hidden">
                         <path strokeLinecap="round" strokeLinejoin="round" d="M5.25 5.653c0-.856.917-1.398 1.667-.986l11.54 6.348a1.125 1.125 0 010 1.971l-11.54 6.347a1.125 1.125 0 01-1.667-.985V5.653z" />
                     </svg>
@@ -217,34 +223,34 @@ export const PageReader = () => {
                         <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 5.25v13.5m-7.5-13.5v13.5" />
                     </svg>
                 </button>
-                <button type="button" title="next" id="next" className="hover:scale-125 focus:outline-none" onClick={() => { next() }}>
+                <button type="button" title="page-reader-next" id="page-reader-next" className="hover:scale-125 focus:outline-none" onClick={() => { next() }}>
                     <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
                         <path strokeLinecap="round" strokeLinejoin="round" d="M3 8.688c0-.864.933-1.405 1.683-.977l7.108 4.062a1.125 1.125 0 010 1.953l-7.108 4.062A1.125 1.125 0 013 16.81V8.688zM12.75 8.688c0-.864.933-1.405 1.683-.977l7.108 4.062a1.125 1.125 0 010 1.953l-7.108 4.062a1.125 1.125 0 01-1.683-.977V8.688z" />
                     </svg>
 
                 </button>
-                <button type="button" title="toggleoptions" id="toggleoptions" className="hover:scale-125 focus:outline-none" onClick={() => { setOptions(!options) }}>
+                <button type="button" title="page-reader-toggleoptions" id="page-reader-toggleoptions" className="hover:scale-125 focus:outline-none" onClick={() => { setOptions(!options) }}>
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
                         <path strokeLinecap="round" strokeLinejoin="round" d="M10.5 6h9.75M10.5 6a1.5 1.5 0 11-3 0m3 0a1.5 1.5 0 10-3 0M3.75 6H7.5m3 12h9.75m-9.75 0a1.5 1.5 0 01-3 0m3 0a1.5 1.5 0 00-3 0m-3.75 0H7.5m9-6h3.75m-3.75 0a1.5 1.5 0 01-3 0m3 0a1.5 1.5 0 00-3 0m-9.75 0h9.75" />
                     </svg>
                 </button>
             </div>
 
-            <div id="options"
+            <div id="page-reader-options"
                 className={"fixed top-80 left-1/2 -translate-x-1/2 flex-wrap bg-gray-800 text-white rounded-lg shadow-2xl shadow-black p-3 " + (options ? "" : "hidden")}>
                 <div className="p-2">
-                    <label htmlFor="speed">Speed:</label>
-                    <select id="speed" className="bg-gray-700 focus:outline-none">
+                    <label htmlFor="page-reader-speed">Speed:</label>
+                    <select id="page-reader-speed" defaultValue="1" className="bg-gray-700 focus:outline-none">
                         <option value='0.5'>0.5x</option>
-                        <option value='1' selected>1x</option>
+                        <option value='1'>1x</option>
                         <option value='1.5'>1.5x</option>
                         <option value='2'>2x</option>
                     </select>
                 </div>
 
                 <div className="p-2">
-                    <label htmlFor="voices">Voices:</label>
-                    <select id="voices" className="bg-gray-700 focus:outline-none"></select>
+                    <label htmlFor="page-reader-voices">Voices:</label>
+                    <select id="page-reader-voices" className="bg-gray-700 focus:outline-none"></select>
                 </div>
             </div>
         </div>
